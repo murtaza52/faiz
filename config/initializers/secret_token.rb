ee8ad9122d83789d432639522bcfcbd99b60a91e
Murtaza Husain <murtaza.husain@searce.com>
2012-08-16 23:17:05 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faiz::Application.config.secret_token = '46735f851d7fae2ddd653663ebcfdc596b8fa843bee33d3dcc614d3fc08a5ede1f3c63c1043423cd89d8dade91e68d21584d76dd27c8343c86a18a8f1b797c71'
